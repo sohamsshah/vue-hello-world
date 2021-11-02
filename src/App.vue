58246d530b7bf0dac1acc53ab11d877c1d57f92c
sohamsshah <sohamshah456@gmail.com>
2021-11-02 18:58:30 +0530

    complete modal project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <!-- <Modal heading="Sign up for the Givaway!" text="Grab your ninja swag for half price!" /> -->
+  <Modal theme="" :showModal='showModal' :showModalTwo='showModalTwo' @closeTwo='toggleModalTwo' @close='toggleModal' >
+    
+    <template v-slot:modalOne>
+      <a href="">Take you to the moon</a>
+      <h2>{{title}}</h2>
+    <p>{{text}} </p>
+    </template>
+    <template v-slot:modalTwo>
+    <p>{{text}} </p>
+    </template>
+  </Modal>
+  <button @click='toggleModal'>Show Modal</button>
+  <button @click='toggleModalTwo'>Show Modal Two</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App!',
+      heading: 'Sign up for the Givaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
+    }
 }
 </script>
 
@@ -22,5 +51,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="backdrop"  v-show="showModal || showModalTwo">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <button @click='handleClose'>Close me</button>
+      <div v-show='showModal'>
+        <slot name="modalOne"></slot>
+      </div>
+      <div v-show='showModalTwo'>
+        <slot name="modalTwo"></slot>
+      </div>
+      
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['theme', 'showModal', 'showModalTwo'],
+  methods: {
+    closeModal(){
+      console.log('here 2')
+      this.$emit('close')
+      },
+    closeModalTwo(){
+      console.log('here')
+      this.$emit('closeTwo')
+      },
+    handleClose(){
+      console.log('called')
+      if(this.showModal){
+        this.closeModal()
+        }else{
+          this.closeModalTwo()
+        }
+      }
+}}
+</script>
+
+<style>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+  }
+  /* sale styles */
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+</style>
